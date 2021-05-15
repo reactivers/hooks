d21c47d11e3299ca842f48c7366405cf06e6171f
Murat Guney <muratguney@Murat-Mac-mini.local>
2021-05-16 00:45:43 +0300

    rebuild

--- src/hooks/useLocales/index.tsx
+++ src/hooks/useLocales/index.tsx
@@ -2,12 +2,27 @@ import { createContext, FC, useContext, useMemo, useState } from "react";
 
 export interface LocalesContextProps<T> {
     locale: T;
-    setActiveLanguage: (lang: string) => void;
+    setActiveLanguage: (lang: Languages) => void;
 }
 
+declare type Languages = "af" | "sq" | "ar-SA" | "ar-IQ" | "ar-EG" | "ar-LY" | "ar-DZ" | "ar-MA" | "ar-TN" | "ar-OM" |
+    "ar-YE" | "ar-SY" | "ar-JO" | "ar-LB" | "ar-KW" | "ar-AE" | "ar-BH" | "ar-QA" | "eu" | "bg" |
+    "be" | "ca" | "zh-TW" | "zh-CN" | "zh-HK" | "zh-SG" | "hr" | "cs" | "da" | "nl" | "nl-BE" | "en" |
+    "en-US" | "en-EG" | "en-AU" | "en-GB" | "en-CA" | "en-NZ" | "en-IE" | "en-ZA" | "en-JM" |
+    "en-BZ" | "en-TT" | "et" | "fo" | "fa" | "fi" | "fr" | "fr-BE" | "fr-CA" | "fr-CH" | "fr-LU" |
+    "gd" | "gd-IE" | "de" | "de-CH" | "de-AT" | "de-LU" | "de-LI" | "el" | "he" | "hi" | "hu" |
+    "is" | "id" | "it" | "it-CH" | "ja" | "ko" | "lv" | "lt" | "mk" | "mt" | "no" | "pl" |
+    "pt-BR" | "pt" | "rm" | "ro" | "ro-MO" | "ru" | "ru-MI" | "sz" | "sr" | "sk" | "sl" | "sb" |
+    "es" | "es-AR" | "es-GT" | "es-CR" | "es-PA" | "es-DO" | "es-MX" | "es-VE" | "es-CO" |
+    "es-PE" | "es-EC" | "es-CL" | "es-UY" | "es-PY" | "es-BO" | "es-SV" | "es-HN" | "es-NI" |
+    "es-PR" | "sx" | "sv" | "sv-FI" | "th" | "ts" | "tn" | "tr" | "uk" | "ur" | "ve" | "vi" | "xh" |
+    "ji" | "zu"
+
 export interface LocalesProviderProps<T> {
-    locales?: Record<string, T>;
-    activeLanguage?: string
+    locales?: {
+        [language in Languages]: T
+    };
+    activeLanguage?: Languages
 }
 
 function createLocale<T>() {
@@ -16,7 +31,7 @@ function createLocale<T>() {
 
     const LocalesProvider: FC<LocalesProviderProps<T>> = ({ locales, activeLanguage: _activeLanguage, children }) => {
 
-        const [activeLanguage, setActiveLanguage] = useState<keyof typeof locales>(_activeLanguage)
+        const [activeLanguage, setActiveLanguage] = useState<Languages>(_activeLanguage)
         const locale = useMemo(() => locales[activeLanguage], [locales, activeLanguage])
 
         return (
