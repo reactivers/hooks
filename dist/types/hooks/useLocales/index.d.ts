09f4284609d93ccbfd9b9c8fa572eb54cb3fd26a
Murat Guney <muratguney@Murat-Mac-mini.local>
2021-05-16 00:30:20 +0300

    rebuild

--- dist/types/hooks/useLocales/index.d.ts
+++ dist/types/hooks/useLocales/index.d.ts
@@ -8,9 +8,7 @@ export interface LocalesContextProps<T> {
     setActiveLanguage: (lang: string) => void;
 }
 export interface LocalesProviderProps<T> {
-    locales?: {
-        [key: string]: T;
-    };
+    locales?: Record<string, T>;
     activeLanguage?: string;
 }
 declare function createLocale<T>(): {
--- src/hooks/useLocales/index.tsx
+++ src/hooks/useLocales/index.tsx
@@ -8,9 +8,7 @@ export interface LocalesContextProps<T> {
 }
 
 export interface LocalesProviderProps<T> {
-    locales?: {
-        [key: string]: T
-    };
+    locales?: Record<string, T>;
     activeLanguage?: string
 }
 
