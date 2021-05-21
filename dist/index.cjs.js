1dba84b53178e4233b2282fc67f541c60e869f28
Murat Guney <muratguney1995@gmail.com>
2021-05-21 22:34:52 +0300

    rebuild

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2479,8 +2479,8 @@ var useGlobalState = function () {
 };
 
 var useClickInside = function (_a) {
-    var ref = _a.ref, callback = _a.callback, _b = _a.withState, withState = _b === void 0 ? false : _b;
-    var _c = react.useState(false), clickedState = _c[0], setClickedState = _c[1];
+    var ref = _a.ref, callback = _a.callback, _b = _a.withState, withState = _b === void 0 ? false : _b, _c = _a.passive, passive = _c === void 0 ? true : _c;
+    var _d = react.useState(false), clickedState = _d[0], setClickedState = _d[1];
     var clickedRef = react.useRef(false);
     var updateSwitch = react.useCallback(function (newValue) {
         if (withState) {
@@ -2502,17 +2502,17 @@ var useClickInside = function (_a) {
         }
     }, [ref.current, callback, updateSwitch]);
     react.useEffect(function () {
-        document.addEventListener("click", onClick);
+        document.addEventListener("click", onClick, { passive: passive });
         return function () {
             document.removeEventListener("click", onClick);
         };
-    }, [onClick]);
+    }, [onClick, passive]);
     return withState ? clickedState : clickedRef.current;
 };
 
 var useClickOutside = function (_a) {
-    var ref = _a.ref, callback = _a.callback, _b = _a.withState, withState = _b === void 0 ? false : _b;
-    var _c = react.useState(false), clickedState = _c[0], setClickedState = _c[1];
+    var ref = _a.ref, callback = _a.callback, _b = _a.withState, withState = _b === void 0 ? false : _b, _c = _a.passive, passive = _c === void 0 ? true : _c;
+    var _d = react.useState(false), clickedState = _d[0], setClickedState = _d[1];
     var clickedRef = react.useRef(false);
     var updateSwitch = react.useCallback(function (newValue) {
         if (withState) {
@@ -2534,11 +2534,11 @@ var useClickOutside = function (_a) {
         }
     }, [ref.current, callback, updateSwitch]);
     react.useEffect(function () {
-        document.addEventListener("click", onClick);
+        document.addEventListener("click", onClick, { passive: passive });
         return function () {
             document.removeEventListener("click", onClick);
         };
-    }, [onClick]);
+    }, [onClick, passive]);
     return withState ? clickedState : clickedRef.current;
 };
 
--- dist/types/hooks/useClickInside/index.d.ts
+++ dist/types/hooks/useClickInside/index.d.ts
@@ -2,6 +2,7 @@ import { MutableRefObject } from "react";
 interface IUseClickInside {
     ref: MutableRefObject<any>;
     callback: (event: MouseEvent | TouchEvent) => void;
+    passive?: boolean;
     withState?: boolean;
 }
 declare const useClickInside: (params: IUseClickInside) => boolean;
