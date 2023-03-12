0b2e97ac27c66d3c61c03c23d2e186ad9a851624
Murat Güney <muratguney1995@gmail.com>
2023-03-12 23:42:32 +0300

    feat(package): versions are upgraded

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2,237 +2,167 @@
 
 Object.defineProperty(exports, '__esModule', { value: true });
 
-var useFetch = require('@reactivers/use-fetch');
 var useAuth = require('@reactivers/use-auth');
-var useDimensions = require('@reactivers/use-dimensions');
-var useEventListener = require('@reactivers/use-event-listener');
-var useLoading = require('@reactivers/use-loading');
-var useLocale = require('@reactivers/use-locale');
-var useSocket = require('@reactivers/use-socket');
-var useSafeArea = require('@reactivers/use-safe-area');
-var useTheme = require('@reactivers/use-theme');
-var useLocalStorage = require('@reactivers/use-local-storage');
-var useCookie = require('@reactivers/use-cookie');
-var useGlobalState = require('@reactivers/use-global-state');
-var useUtils = require('@reactivers/use-utils');
 var useClickInside = require('@reactivers/use-click-inside');
 var useClickOutside = require('@reactivers/use-click-outside');
-var usePrevious = require('@reactivers/use-previous');
-var useMounted = require('@reactivers/use-mounted');
-var useTitle = require('@reactivers/use-title');
-var useMeasure = require('@reactivers/use-measure');
-var useHover = require('@reactivers/use-hover');
+var useCookie = require('@reactivers/use-cookie');
 var useCounter = require('@reactivers/use-counter');
+var useDimensions = require('@reactivers/use-dimensions');
+var useEventListener = require('@reactivers/use-event-listener');
+var useFetch = require('@reactivers/use-fetch');
+var useGlobalState = require('@reactivers/use-global-state');
+var useHover = require('@reactivers/use-hover');
+var useLoading = require('@reactivers/use-loading');
+var useLocalStorage = require('@reactivers/use-local-storage');
+var useLocale = require('@reactivers/use-locale');
+var useMeasure = require('@reactivers/use-measure');
+var useMounted = require('@reactivers/use-mounted');
+var usePrevious = require('@reactivers/use-previous');
+var useSafeArea = require('@reactivers/use-safe-area');
+var useSocket = require('@reactivers/use-socket');
+var useTheme = require('@reactivers/use-theme');
+var useTitle = require('@reactivers/use-title');
+var useUtils = require('@reactivers/use-utils');
 
 
 
-Object.defineProperty(exports, 'FetchProvider', {
-	enumerable: true,
-	get: function () {
-		return useFetch.FetchProvider;
-	}
-});
-Object.defineProperty(exports, 'useDelete', {
-	enumerable: true,
-	get: function () {
-		return useFetch.useDelete;
-	}
-});
-Object.defineProperty(exports, 'useFetch', {
-	enumerable: true,
-	get: function () {
-		return useFetch.useFetch;
-	}
-});
-Object.defineProperty(exports, 'useGet', {
-	enumerable: true,
-	get: function () {
-		return useFetch.useGet;
-	}
-});
-Object.defineProperty(exports, 'usePost', {
-	enumerable: true,
-	get: function () {
-		return useFetch.usePost;
-	}
-});
-Object.defineProperty(exports, 'usePut', {
-	enumerable: true,
-	get: function () {
-		return useFetch.usePut;
-	}
-});
 Object.defineProperty(exports, 'AuthProvider', {
-	enumerable: true,
-	get: function () {
-		return useAuth.AuthProvider;
-	}
+  enumerable: true,
+  get: function () { return useAuth.AuthProvider; }
 });
 Object.defineProperty(exports, 'useAuth', {
-	enumerable: true,
-	get: function () {
-		return useAuth.useAuth;
-	}
-});
-Object.defineProperty(exports, 'DimensionsProvider', {
-	enumerable: true,
-	get: function () {
-		return useDimensions.DimensionsProvider;
-	}
-});
-Object.defineProperty(exports, 'useDimensions', {
-	enumerable: true,
-	get: function () {
-		return useDimensions.useDimensions;
-	}
-});
-Object.defineProperty(exports, 'EventListenerProvider', {
-	enumerable: true,
-	get: function () {
-		return useEventListener.EventListenerProvider;
-	}
-});
-Object.defineProperty(exports, 'useEventListener', {
-	enumerable: true,
-	get: function () {
-		return useEventListener.useEventListener;
-	}
-});
-Object.defineProperty(exports, 'LoadingProvider', {
-	enumerable: true,
-	get: function () {
-		return useLoading.LoadingProvider;
-	}
-});
-Object.defineProperty(exports, 'useLoading', {
-	enumerable: true,
-	get: function () {
-		return useLoading.useLoading;
-	}
-});
-Object.defineProperty(exports, 'createLocale', {
-	enumerable: true,
-	get: function () {
-		return useLocale.createLocale;
-	}
-});
-Object.defineProperty(exports, 'SocketProvider', {
-	enumerable: true,
-	get: function () {
-		return useSocket.SocketProvider;
-	}
-});
-Object.defineProperty(exports, 'useSocket', {
-	enumerable: true,
-	get: function () {
-		return useSocket.useSocket;
-	}
-});
-Object.defineProperty(exports, 'SafeAreaProvider', {
-	enumerable: true,
-	get: function () {
-		return useSafeArea.SafeAreaProvider;
-	}
-});
-Object.defineProperty(exports, 'useSafeArea', {
-	enumerable: true,
-	get: function () {
-		return useSafeArea.useSafeArea;
-	}
-});
-Object.defineProperty(exports, 'createTheme', {
-	enumerable: true,
-	get: function () {
-		return useTheme.createTheme;
-	}
-});
-Object.defineProperty(exports, 'LocalStorageProvider', {
-	enumerable: true,
-	get: function () {
-		return useLocalStorage.LocalStorageProvider;
-	}
-});
-Object.defineProperty(exports, 'useLocalStorage', {
-	enumerable: true,
-	get: function () {
-		return useLocalStorage.useLocalStorage;
-	}
-});
-Object.defineProperty(exports, 'CookieProvider', {
-	enumerable: true,
-	get: function () {
-		return useCookie.CookieProvider;
-	}
-});
-Object.defineProperty(exports, 'useCookie', {
-	enumerable: true,
-	get: function () {
-		return useCookie.useCookie;
-	}
-});
-Object.defineProperty(exports, 'GlobalStateProvider', {
-	enumerable: true,
-	get: function () {
-		return useGlobalState.GlobalStateProvider;
-	}
-});
-Object.defineProperty(exports, 'useGlobalState', {
-	enumerable: true,
-	get: function () {
-		return useGlobalState.useGlobalState;
-	}
-});
-Object.defineProperty(exports, 'useUtils', {
-	enumerable: true,
-	get: function () {
-		return useUtils.useUtils;
-	}
+  enumerable: true,
+  get: function () { return useAuth.useAuth; }
 });
 Object.defineProperty(exports, 'useClickInside', {
-	enumerable: true,
-	get: function () {
-		return useClickInside.useClickInside;
-	}
+  enumerable: true,
+  get: function () { return useClickInside.useClickInside; }
 });
 Object.defineProperty(exports, 'useClickOutside', {
-	enumerable: true,
-	get: function () {
-		return useClickOutside.useClickOutside;
-	}
+  enumerable: true,
+  get: function () { return useClickOutside.useClickOutside; }
 });
-Object.defineProperty(exports, 'usePrevious', {
-	enumerable: true,
-	get: function () {
-		return usePrevious.usePrevious;
-	}
+Object.defineProperty(exports, 'CookieProvider', {
+  enumerable: true,
+  get: function () { return useCookie.CookieProvider; }
 });
-Object.defineProperty(exports, 'useMounted', {
-	enumerable: true,
-	get: function () {
-		return useMounted.useMounted;
-	}
-});
-Object.defineProperty(exports, 'useTitle', {
-	enumerable: true,
-	get: function () {
-		return useTitle.useTitle;
-	}
-});
-Object.defineProperty(exports, 'useMeasure', {
-	enumerable: true,
-	get: function () {
-		return useMeasure.useMeasure;
-	}
-});
-Object.defineProperty(exports, 'useHover', {
-	enumerable: true,
-	get: function () {
-		return useHover.useHover;
-	}
+Object.defineProperty(exports, 'useCookie', {
+  enumerable: true,
+  get: function () { return useCookie.useCookie; }
 });
 Object.defineProperty(exports, 'useCounter', {
-	enumerable: true,
-	get: function () {
-		return useCounter.useCounter;
-	}
+  enumerable: true,
+  get: function () { return useCounter.useCounter; }
+});
+Object.defineProperty(exports, 'DimensionsProvider', {
+  enumerable: true,
+  get: function () { return useDimensions.DimensionsProvider; }
+});
+Object.defineProperty(exports, 'useDimensions', {
+  enumerable: true,
+  get: function () { return useDimensions.useDimensions; }
+});
+Object.defineProperty(exports, 'EventListenerProvider', {
+  enumerable: true,
+  get: function () { return useEventListener.EventListenerProvider; }
+});
+Object.defineProperty(exports, 'useEventListener', {
+  enumerable: true,
+  get: function () { return useEventListener.useEventListener; }
+});
+Object.defineProperty(exports, 'FetchProvider', {
+  enumerable: true,
+  get: function () { return useFetch.FetchProvider; }
+});
+Object.defineProperty(exports, 'useDelete', {
+  enumerable: true,
+  get: function () { return useFetch.useDelete; }
+});
+Object.defineProperty(exports, 'useFetch', {
+  enumerable: true,
+  get: function () { return useFetch.useFetch; }
+});
+Object.defineProperty(exports, 'useGet', {
+  enumerable: true,
+  get: function () { return useFetch.useGet; }
+});
+Object.defineProperty(exports, 'usePost', {
+  enumerable: true,
+  get: function () { return useFetch.usePost; }
+});
+Object.defineProperty(exports, 'usePut', {
+  enumerable: true,
+  get: function () { return useFetch.usePut; }
+});
+Object.defineProperty(exports, 'GlobalStateProvider', {
+  enumerable: true,
+  get: function () { return useGlobalState.GlobalStateProvider; }
+});
+Object.defineProperty(exports, 'useGlobalState', {
+  enumerable: true,
+  get: function () { return useGlobalState.useGlobalState; }
+});
+Object.defineProperty(exports, 'useHover', {
+  enumerable: true,
+  get: function () { return useHover.useHover; }
+});
+Object.defineProperty(exports, 'LoadingProvider', {
+  enumerable: true,
+  get: function () { return useLoading.LoadingProvider; }
+});
+Object.defineProperty(exports, 'useLoading', {
+  enumerable: true,
+  get: function () { return useLoading.useLoading; }
+});
+Object.defineProperty(exports, 'LocalStorageProvider', {
+  enumerable: true,
+  get: function () { return useLocalStorage.LocalStorageProvider; }
+});
+Object.defineProperty(exports, 'useLocalStorage', {
+  enumerable: true,
+  get: function () { return useLocalStorage.useLocalStorage; }
+});
+Object.defineProperty(exports, 'createLocale', {
+  enumerable: true,
+  get: function () { return useLocale.createLocale; }
+});
+Object.defineProperty(exports, 'useMeasure', {
+  enumerable: true,
+  get: function () { return useMeasure.useMeasure; }
+});
+Object.defineProperty(exports, 'useMounted', {
+  enumerable: true,
+  get: function () { return useMounted.useMounted; }
+});
+Object.defineProperty(exports, 'usePrevious', {
+  enumerable: true,
+  get: function () { return usePrevious.usePrevious; }
+});
+Object.defineProperty(exports, 'SafeAreaProvider', {
+  enumerable: true,
+  get: function () { return useSafeArea.SafeAreaProvider; }
+});
+Object.defineProperty(exports, 'useSafeArea', {
+  enumerable: true,
+  get: function () { return useSafeArea.useSafeArea; }
+});
+Object.defineProperty(exports, 'SocketProvider', {
+  enumerable: true,
+  get: function () { return useSocket.SocketProvider; }
+});
+Object.defineProperty(exports, 'useSocket', {
+  enumerable: true,
+  get: function () { return useSocket.useSocket; }
+});
+Object.defineProperty(exports, 'createTheme', {
+  enumerable: true,
+  get: function () { return useTheme.createTheme; }
+});
+Object.defineProperty(exports, 'useTitle', {
+  enumerable: true,
+  get: function () { return useTitle.useTitle; }
+});
+Object.defineProperty(exports, 'useUtils', {
+  enumerable: true,
+  get: function () { return useUtils.useUtils; }
 });
